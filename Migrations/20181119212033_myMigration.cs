e6cf0416af4f1e4ac2e0b1c59097b7826d4af1a9
mdchaff <41088092+mdchaff@users.noreply.github.com>
2018-11-19 16:18:59 -0600

    Add files via upload

---
+++ Migrations/20181119212033_myMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace angularAutoSite.Migrations
+{
+    public partial class myMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AngAutos",
+                columns: table => new
+                {
+                    AngAuto_ID = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    projectName = table.Column<string>(nullable: true),
+                    thePath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AngAutos", x => x.AngAuto_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Installs",
+                columns: table => new
+                {
+                    Install_ID = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    AngAuto_ID = table.Column<int>(nullable: false),
+                    TheInstall = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Installs", x => x.Install_ID);
+                    table.ForeignKey(
+                        name: "FK_Installs_AngAutos_AngAuto_ID",
+                        column: x => x.AngAuto_ID,
+                        principalTable: "AngAutos",
+                        principalColumn: "AngAuto_ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Installs_AngAuto_ID",
+                table: "Installs",
+                column: "AngAuto_ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Installs");
+
+            migrationBuilder.DropTable(
+                name: "AngAutos");
+        }
+    }
+}
